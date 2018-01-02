3082583b6e23e040403a2aee782bd386f3f3e308
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-01-02 15:33:27 -0500

    improved searcher to recursively search through jsons rather than html

--- renderer/main-window/js/searcher.js
+++ renderer/main-window/js/searcher.js
@@ -5,17 +5,30 @@ function searchPmrs() {
 
     Array.from(widgets, wgt => {
         // Search cache.
-        fse.readFile(__dirname + '/renderer/main-window/cache/' + addCommas(wgt.dataset.section) + '.html', function(err, data) {
+        fse.readFile(__dirname + '/db/' + addCommas(wgt.dataset.section) + '/pmr.json', function(err, fdata) {
             if (err) {
-                console.error('readFile: Failed to open pmr html for searching ' + wgt.dataset.section);
+                console.error('readFile: Failed to open file for editing');
                 console.error(err);
             } else {
-                if (data.indexOf(filter) > -1) {
+                if (_containsValue(JSON.parse(fdata), filter)) {
                     wgt.style.display = '';
                 } else {
-                    wgt.style.display = 'none';
+                    wgt.style.display = 'none'
                 }
             }
         });
     });
+}
+
+function _containsValue(json, value) {
+    for (k in json) {
+        if (typeof json[k] === 'object') {
+            if (_containsValue(json[k], value)) {
+                return true;
+            }
+        } else if (typeof json[k] === 'string' && json[k].toLowerCase().indexOf(value) > -1) {
+            return true;
+        }
+    }
+    return false;
 }
