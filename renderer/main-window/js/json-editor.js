0b8860d174b993d7e375ae3fdd98b9167b9ae564
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-02-07 13:00:58 -0500

    renamed

--- renderer/main-window/js/json-editor.js
+++ renderer/main-window/js/json-editor.js
@@ -3,13 +3,13 @@
  * function makes changes to the json and returns it. The result is then written to 
  * savePath. An optional callback can be passed in and will execute after the write.
  */
-function jsonEditor(filePath, savePath, editFunction, callback) {
+function jsonEditor(filePath, savePath, editf, callback) {
     fse.readFile(filePath, function(err, fdata) {
         if (err) {
             console.error('readFile: Failed to open file for editing ' + filePath);
             console.error(err);
         } else {
-            var json = editFunction(JSON.parse(fdata));
+            var json = editf(JSON.parse(fdata));
             fse.writeFile(savePath, JSON.stringify(json, null, 4), function(err) {
                 if (err) {
                     console.error('writeFile: Failed to write file ' + savePath);
